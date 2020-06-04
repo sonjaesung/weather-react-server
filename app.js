4669c160bbeac7d5b9ce28e31f6bab195e87d531
sonjaesung <loki92@naver.com>
2020-06-04 17:04:33 +0900

    3001 test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+    origin: 'http://localhost:3001', // 허락하고자 하는 요청 주소
     credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
 };
 
