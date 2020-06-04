93dcf0b6d2f7c0e5af3af637c3b688f1946d7c5d
sonjaesung <loki92@naver.com>
2020-06-04 17:09:59 +0900

    aws cors test

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -12,7 +12,7 @@ const verifyToken = (req, res, next) => {
             res.locals.seq = decoded.userSeq;
             next();
         } else {
-            res.status(401).json({ error: 'unauthorized' });
+            res.status(401).json({ error: 'unauthorized' + req.cookies.user });
         }
 
     } catch (err) {
