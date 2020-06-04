4183e205c4005027ada121bd37beaf029632042f
sonjaesung <loki92@naver.com>
2020-06-04 14:14:35 +0900

    todayEnglish router add

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ let router = express.Router();
 
 let loginRouter = require("./login");
 let joinRouter = require("./join");
+let englishRouter = require('./todayEnglish');
 
 router.use("/login", loginRouter);
 router.use("/join", joinRouter);
+router.use("/todayEnglish", englishRouter);
 
 module.exports = router;
