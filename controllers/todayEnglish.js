04b0bd7f74ef18c290ef690b86266b7f4e301e8a
sonjaesung <loki92@naver.com>
2020-06-05 15:37:34 +0900

    한쪽으로 통합

--- controllers/todayEnglish.js
+++ controllers/todayEnglish.js
@@ -4,7 +4,16 @@ const TodayEnglish = db.TodayEnglish;
 exports.get = async (req, res) => {
     console.log(res.locals.id);
 
-    return res.json('ok');
+    const clientToken = req.cookies.user;
+    const decoded = jwt.verify(clientToken, secretObj.secret);
+
+    if (decoded) {
+        res.locals.id = decoded.email;
+        res.locals.seq = decoded.userSeq;
+        return res.json('ok');
+    } else {
+        return res.json('no');
+    }
 }
 
 exports.add = async (req, res) => {
