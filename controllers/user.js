114189bcd98fe3ce50102c86470b2be1ffb83376
sonjaesung <loki92@naver.com>
2020-06-08 13:29:44 +0900

    login get

--- controllers/user.js
+++ controllers/user.js
@@ -7,6 +7,10 @@ const User = db.User;
 
 let key = 'salt';
 
+exports.get = async (req, res) => {
+    return res.json('ok');
+}
+
 exports.login = async (req, res) => {
     let data = req.body;
     
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 var userController = require("../controllers/user");
+var auth = require('../controllers/authorization');
 
+router.get("/", auth.verifyToken, userController.get);
 router.post("/", userController.login);
 
 module.exports = router;
